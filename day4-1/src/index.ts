c3ffd3c146cb89c5cf0cad13dbec5994afb5f4cc
Niek Nijland <ngnijland@gmail.com>
2019-12-06 10:11:18 +0100

    add day 4 part 1

---
+++ day4-1/src/index.ts
@@ -0,0 +1,72 @@
+const assert = require("assert");
+
+const range = [353096, 843212];
+
+let validPasswordCount = 0;
+
+function hasAdjacentDigits(list: string[]): boolean {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === list[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function digitsIncrease(list: string[]): boolean {
+  for (let i = 0; i < list.length; i++) {
+    if (parseInt(list[i]) > parseInt(list[i + 1])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+for (let i = range[0] + 1; i < range[1]; i++) {
+  const charArray = i.toString().split("");
+
+  if (charArray.length !== 6) {
+    continue;
+  }
+
+  if (!hasAdjacentDigits(charArray)) {
+    continue;
+  }
+
+  if (!digitsIncrease(charArray)) {
+    continue;
+  }
+
+  validPasswordCount++;
+}
+
+assert.strictEqual(
+  hasAdjacentDigits(["1", "2", "3", "4"]),
+  false,
+  "Array with no adjescent numbers should return false"
+);
+assert.strictEqual(
+  hasAdjacentDigits(["1", "2", "2", "3"]),
+  true,
+  "Array with adjescent numbers should return true"
+);
+
+assert.strictEqual(
+  digitsIncrease(["1", "2", "3"]),
+  true,
+  "Array with only increasing digits should return true"
+);
+assert.strictEqual(
+  digitsIncrease(["1", "2", "2"]),
+  true,
+  "Array with increasing or equal digits should return true"
+);
+assert.strictEqual(
+  digitsIncrease(["1", "2", "1"]),
+  false,
+  "Array with decreasing digits should return false"
+);
+
+console.log(validPasswordCount);
